2f99ad06ddd4c167e656d1d6bb9ef061d917a994
LJM12914 <LJM12914@outlook.com>
2023-03-04 14:00:45 +0800

    0.1.1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+﻿import React, { Component as Cp } from "react";
+type State = Immutable<{
+    statusArray :[
+        
+    ];
+}>;
+export default class Tac extends Cp<{}, State>{
+    
+    render() :React.ReactNode{
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
---
+++ src/main.d.ts
@@ -0,0 +1,15 @@
+﻿type anyObject = Record<string, any>;
+//#region 此处代码来自Immer：https://github.com/immerjs/immer。Immer 以 MIT License 开源：https://github.com/immerjs/immer/blob/main/LICENSE
+type IfAvailable<T, Fallback = void> =
+    boolean extends (T extends never ? true : false) ? Fallback
+        : keyof T extends never ? Fallback
+            : T;
+type Immutable<T> =
+    T extends (string | number | boolean) ? T
+        : T extends (Function | RegExp | Promise<any> | Date) ? T
+            : T extends IfAvailable<ReadonlyMap<infer K, infer V>> ? ReadonlyMap<Immutable<K>, Immutable<V>>
+                : T extends IfAvailable<ReadonlySet<infer V>> ? ReadonlySet<Immutable<V>>
+                    : T extends (WeakMap<any, any> | WeakSet<any>) ? T
+                        : T extends object ? {readonly [K in keyof T]: Immutable<T[K]>;}
+                            : T;
+//#endregion
